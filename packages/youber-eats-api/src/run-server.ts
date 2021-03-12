37878b54bf0841672b7a85eb214d3622e76267b1
David Letaw <jdletaw@gmail.com>
2021-03-12 01:11:13 -0800

    Connects axios and webhook in slack

--- packages/youber-eats-api/src/run-server.ts
+++ packages/youber-eats-api/src/run-server.ts
@@ -1,11 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import bodyParser from "body-parser";
+import axios from 'axios';
 
-dotenv.config(); // load a .env file if it exists, override process.env values
+
+dotenv.config(); // load a .env file if it exists, sets/override process.env values
 
 const port = process.env.PORT || 8080;
 const app = express();
+
+const incomingWebhookUrl = process.env.BOT_SLACK_WEBHOOK_URL;
 
 app.use(bodyParser.json());
 
@@ -14,9 +18,6 @@ app.get('/online', (req, res) => {
 });
 
 app.post('/slack-events', (req, res) => {
-    // TODO: get the challenge value
-    console.log(req.body);
-    res.send(req.body.challenge);
 });
 
 app.listen(port, () => {
